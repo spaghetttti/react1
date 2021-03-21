6bb521c04082345349508c95d68be30a0aa12a55
AsilM <asil9802mum@gmail.com>
2021-03-21 16:12:05 +0500

    design changes

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,10 @@ import './card.styles.css';
 // eslint-disable-next-line
 export const Card = (props) => ( 
     <div className = 'card-container'>
+        <div className="trans">
         <img src={`https://robohash.org/KKY.png${props.monster.id}?set=set1&size=180x180`} alt="robot"/>
         <h5> <i> {props.monster.name} </i></h5>
-        <p> { props.monster.email } </p>
+        <p> { props.monster.email } </p> 
+        </div>
     </div>
 )
